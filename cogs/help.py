6a2a13e69a7d657e25b036e2d8c2c443c81eddd7
RebelsChild <dyumopyruoo@gmail.com>
2023-07-05 23:42:02 +0700

    nothing updated

--- cogs/help.py
+++ cogs/help.py
@@ -8,7 +8,7 @@ class help(commands.Cog):
     @commands.command()
     async def help(self, ctx):
         em = discord.Embed(
-                description=f"hi this command is to show all the features of this bot",
+                description=f"The '.help' command provides help and information about the available commands.",
                 colour=discord.Colour.random(),
                 timestamp=ctx.message.created_at
                 )
@@ -17,7 +17,7 @@ class help(commands.Cog):
         em.add_field(name="Moderator", value="Kick, Ban, clear, unban")
         em.add_field(name="Fun", value="eightball, suwit, profile, say")
         em.add_field(name="Anime", value="waifu, maid, uniform, baal, neko, kitsune, schara, sanime,")
-        em.add_field(name="Other", value="sinfo, getip, iptrack, uinfo, dm, ai, weather")
+        em.add_field(name="Other", value="sinfo, getip, iptrack, uinfo, dm, weather")
         em.set_footer(text=f"{ctx.author.display_name} requests this embed")
         await ctx.send(embed=em)
 
