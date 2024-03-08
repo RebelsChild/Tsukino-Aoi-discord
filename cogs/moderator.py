f7ab6cbfd2a4fec326f268356cef307a4536b079
RebelsChild <110272384+RebelsChild@users.noreply.github.com>
2024-03-08 08:35:32 +0700

    Update moderator.py
    Some changes

--- cogs/moderator.py
+++ cogs/moderator.py
@@ -1,45 +1,45 @@
 import discord
 from discord.ext import commands
 
-class moderator(commands.Cog):
+class Moderator(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(member:discord.Member=None)
-    async def kick(self, ctx, member:discord.Member, *, reason=None):
+    @commands.command()
+    @commands.has_permissions(kick_members=True)
+    async def kick(self, ctx, member: discord.Member, *, reason=None):
         await member.kick(reason=reason)
         await ctx.send(f"{member} successfully kicked")
-        if member.bot == True:
-            pass
-        elif member.bot == False:
+        if not member.bot:
             dm = await member.create_dm()
-            await dm.send(f"you has been kicked from {ctx.guild.name} server with reason {reason}")
-
-    @commands.command(member:discord.Member=None)
-    async def ban(self, ctx, member:discord.Member, *, reason=None):
-        await member.ban(reason=reason)
-        await ctx.send(f"{member} successfully baned")
-        if member.bot == True:
-            pass
-        elif member.bot == False:
-            dm = await member.create_dm()
-            await dm.send(f"you has been banned from {ctx.guild.name} server with reason {reason}")
+            await dm.send(f"You have been kicked from {ctx.guild.name} server with reason: {reason}")
 
     @commands.command()
-    async def unban(self, ctx, *, member):
-        list_ban = ctx.guild.bans()
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, ctx, member: discord.Member, *, reason=None):
+        await member.ban(reason=reason)
+        await ctx.send(f"{member} successfully banned")
+        if not member.bot:
+            dm = await member.create_dm()
+            await dm.send(f"You have been banned from {ctx.guild.name} server with reason: {reason}")
 
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def unban(self, ctx, *, member):
+        bans = await ctx.guild.bans()
         name, discriminator = member.split("#")
-        async for bans in list_ban:
-            user = bans.user
+        for ban_entry in bans:
+            user = ban_entry.user
             if (user.name, user.discriminator) == (name, discriminator):
                 await ctx.guild.unban(user)
                 await ctx.send("The user has been successfully unbanned from the server")
-    
+                break
+
     @commands.command()
-    async def clear(self, ctx, amount=1):
-        await ctx.send("message successfully deleted")
-        await ctx.channel.purge(limit=amount+2)
-        
+    @commands.has_permissions(manage_messages=True)
+    async def clear(self, ctx, amount: int = 1):
+        await ctx.channel.purge(limit=amount+1)
+        await ctx.send("Messages successfully deleted", delete_after=5)
+
 async def setup(bot):
-    await bot.add_cog(moderator(bot))
+    await bot.add_cog(Moderator(bot))
